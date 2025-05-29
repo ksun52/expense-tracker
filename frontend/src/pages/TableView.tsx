854624515936f44360c18a14a9c739ea2b9d4d9d
krsun <krsun@umich.edu>
2025-05-29 11:42:29 -0400

    finished setting up table

---
+++ frontend/src/pages/TableView.tsx
@@ -0,0 +1,23 @@
+import { Payment, columns } from "../components/layout/payments-table/columns"
+import DataTable from "../components/layout/payments-table/data-table"
+import { useEffect, useState } from "react"
+import paymentData from "../data/payment_data.json"
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return paymentData as Payment[]
+}
+
+export default function TableView() {
+  const [data, setData] = useState<Payment[]>([])
+
+  useEffect(() => {
+    getData().then(setData)
+  }, [])
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
