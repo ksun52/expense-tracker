a236ca9d839bd51310bac6e1a0ca559de914dfde
krsun <krsun@umich.edu>
2025-06-02 12:27:37 -0400

    added example graph

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import Home from './pages/Home';
 import TableView from './pages/TableView';
 import ChartView from './pages/ChartView';
 import ChartPieLabel from './pages/temp';
+import Graph from './pages/Graph';
+
 import './App.css'
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/table" element={<TableView />} />
           <Route path="/charts" element={<ChartView />} />
+          <Route path="/graph" element={<Graph />} />
           <Route path="/temp" element={<ChartPieLabel />} />
         </Routes>
       </Layout>
